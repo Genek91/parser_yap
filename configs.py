6272383ca85933e668b9b012e7f5b8e64cee0afd
Genek91 <newtowngenek@gmail.com>
2023-10-31 16:29:23 +0300

    cache, prettytable

--- configs.py
+++ configs.py
@@ -8,4 +8,17 @@ def configure_argument_parser(available_modes):
         choices=available_modes,
         help='Режимы работы парсера'
     )
+    parser.add_argument(
+        '-c',
+        '--clear-cache',
+        action='store_true',
+        help='Очистка кеша'
+    )
+    parser.add_argument(
+        '-p',
+        '--pretty',
+        action='store_true',
+        help='Вывод в формате PrettyTable'
+    )
+
     return parser
